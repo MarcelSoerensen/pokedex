731998832f15313203dc402261e38a15193a0587
MarcelSoerensen <marcel.soerensen@icloud.com>
2025-03-03 14:37:42 +0100

    %*

--- scripts/loading-spinner.js
+++ scripts/loading-spinner.js
@@ -1,3 +1,7 @@
+/**
+ * This Function is used to display the loading Spinner
+ */
+
 function displayLoadingSpinner() {
     document.getElementById('loadingSpinner').classList.remove('d-none');
     document.getElementById('morePokemonsButton').classList.add('d-none');
@@ -7,3 +11,4 @@ function displayLoadingSpinner() {
 function hideLoadingSpinner() {
     document.getElementById('loadingSpinner').classList.add('d-none');
 }
+
